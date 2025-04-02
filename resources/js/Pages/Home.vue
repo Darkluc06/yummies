74151d393d59f7c8c55d03a2d91604cf765fd52b
Milou Geervliet <milouyg@MacBook-Air-van-Milou.local>
2025-04-02 09:30:19 +0200

    Created the header in the home page

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,9 +1,3 @@
-<template>
-
-        <navigation></navigation>
-
-</template>
-
 <script>
 import Navigation from '@/Components/navigation/navigation.vue';
 import "../../scss/style.scss"
@@ -14,3 +8,22 @@ export default {
     }
 };
 </script>
+
+<template>
+
+    <article class="yummies">
+        <header class="yummies_header">
+            <div class="yummies_header-content">
+                <h1 class="yummies_header-h1">Yummies</h1>
+                <h2 class="yummies_header-h2">Verschillende kookstijlen</h2>
+            </div>
+            <navigation></navigation>
+        </header>
+
+    </article>
+
+
+
+</template>
+
+
