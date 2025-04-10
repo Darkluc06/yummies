2a93b3f705d1b2660d7d9b703241bb2f5d71e2b8
Jacob Wassall-jamieson <jacob@thewj5.com>
2025-04-10 09:47:38 +0200

    added big arrow

--- resources/js/Components/cookingDiagram/line.vue
+++ resources/js/Components/cookingDiagram/line.vue
@@ -1,24 +1,20 @@
 <template>
     <div class="cookingDiagram__column">
         <div class="cookingDiagram__lineWrapper">
-            <figure class="cookingDiagram__line" :class="active ? `cookingDiagram__line--active` : ``, end ? `cookingDiagram__line--end` : `` ">
+            <figure class="cookingDiagram__line"
+                :class="active ? `cookingDiagram__line--active` : ``, end ? `cookingDiagram__line--end` : ``">
                 <SvgIcon v-if="end === true" :name="`arrow-long`" />
             </figure>
-            <svg xmlns="http://www.w3.org/2000/svg" width="95.5" height="39" viewBox="0 0 95.5 39" v-if="hasBigArrow === true">
-                <g id="Arrow-big" transform="translate(-274.5 -323.5)">
-                    <g id="Rectangle_98" data-name="Rectangle 98" transform="translate(294 337)" fill="none"
-                        stroke="#c3c3c3" stroke-width="1">
-                        <rect width="76" height="12" stroke="none" />
-                        <rect x="0.5" y="0.5" width="75" height="11" fill="none" />
-                    </g>
-                    <g id="Polygon_7" data-name="Polygon 7" transform="translate(274.5 362.5) rotate(-90)" fill="none">
+            <figure class="cookingDiagram__bigArrow" v-if="hasBigArrow === true">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20.5" height="39" viewBox="0 0 20.5 39">
+                    <g id="Polygon_7" data-name="Polygon 7" transform="translate(0 39) rotate(-90)" fill="none">
                         <path d="M19.5,0,39,20.5H0Z" stroke="none" />
                         <path
                             d="M 19.5 1.450927734375 L 2.331371307373047 19.5 L 36.66863250732422 19.5 L 19.5 1.450927734375 M 19.5 0 L 39 20.5 L 0 20.5 L 19.5 0 Z"
-                            stroke="none" fill="#c3c3c3" />
+                            stroke="none" fill="#000" />
                     </g>
-                </g>
-            </svg>
+                </svg>
+            </figure>
             <SvgIcon v-if="hasSmallArrow === true" :name="`arrow-long`" />
         </div>
     </div>
@@ -46,12 +42,12 @@ export default {
             default: false,
             required: false
         },
-        end:{
+        end: {
             type: Boolean,
             default: false
         }
     },
-    components:{
+    components: {
         SvgIcon
     }
 }
