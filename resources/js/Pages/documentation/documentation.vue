636e46bf42ca31e00a094d3876837c650c58f017
Milou Geervliet <milouyg@MacBook-Air-van-Milou.local>
2025-04-10 13:29:32 +0200

    Added styling to documentation

--- resources/js/Pages/documentation/documentation.vue
+++ resources/js/Pages/documentation/documentation.vue
@@ -2,44 +2,16 @@
     <div class="instruction">
         <article id="instruction_documentation" class="instruction_documentation" v-if="showDocumentation">
             <section class="instruction_documentation-top">
-                <header class="instruction_header">
+                <header class="instruction_documentation-header">
                     <h1 class="instruction_documentation-h1">Documentatie</h1>
-                    <Link :href="`/`"><SvgIcon :name="`close`" /></Link>
+                    <Link class="instruction_documentation-close" :href="`/`"><SvgIcon :name="`close`" /></Link>
                 </header>
                 <button class="instruction_documentation-button" @click="goToDocumentation">Documentatie</button>
                 <button class="instruction_legenda-button" @click="goToLegenda">Legenda</button>
             </section>
             <section class="instructions_documentation-mid">
                 <p class="instructions_documentation-p">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae optio voluptates non et a ullam nam qui nihil aliquid fugit tempore expedita dolor nemo unde autem, tenetur nobis debitis deleniti.
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae optio voluptates non et a ullam nam qui nihil aliquid fugit tempore expedita dolor nemo unde autem, tenetur nobis debitis deleniti.
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae optio voluptates non et a ullam nam qui nihil aliquid fugit tempore expedita dolor nemo unde autem, tenetur nobis debitis deleniti.
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae optio voluptates non et a ullam nam qui nihil aliquid fugit tempore expedita dolor nemo unde autem, tenetur nobis debitis deleniti.
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae optio voluptates non et a ullam nam qui nihil aliquid fugit tempore expedita dolor nemo unde autem, tenetur nobis debitis deleniti.
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae optio voluptates non et a ullam nam qui nihil aliquid fugit tempore expedita dolor nemo unde autem, tenetur nobis debitis deleniti.
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae optio voluptates non et a ullam nam qui nihil aliquid fugit tempore expedita dolor nemo unde autem, tenetur nobis debitis deleniti.
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae optio voluptates non et a ullam nam qui nihil aliquid fugit tempore expedita dolor nemo unde autem, tenetur nobis debitis deleniti.
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae optio voluptates non et a ullam nam qui nihil aliquid fugit tempore expedita dolor nemo unde autem, tenetur nobis debitis deleniti.
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae optio voluptates non et a ullam nam qui nihil aliquid fugit tempore expedita dolor nemo unde autem, tenetur nobis debitis deleniti.
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae optio voluptates non et a ullam nam qui nihil aliquid fugit tempore expedita dolor nemo unde autem, tenetur nobis debitis deleniti.
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae optio voluptates non et a ullam nam qui nihil aliquid fugit tempore expedita dolor nemo unde autem, tenetur nobis debitis deleniti.
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae optio voluptates non et a ullam nam qui nihil aliquid fugit tempore expedita dolor nemo unde autem, tenetur nobis debitis deleniti.
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae optio voluptates non et a ullam nam qui nihil aliquid fugit tempore expedita dolor nemo unde autem, tenetur nobis debitis deleniti.
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae optio voluptates non et a ullam nam qui nihil aliquid fugit tempore expedita dolor nemo unde autem, tenetur nobis debitis deleniti.
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae optio voluptates non et a ullam nam qui nihil aliquid fugit tempore expedita dolor nemo unde autem, tenetur nobis debitis deleniti.
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae optio voluptates non et a ullam nam qui nihil aliquid fugit tempore expedita dolor nemo unde autem, tenetur nobis debitis deleniti.
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae optio voluptates non et a ullam nam qui nihil aliquid fugit tempore expedita dolor nemo unde autem, tenetur nobis debitis deleniti.
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae optio voluptates non et a ullam nam qui nihil aliquid fugit tempore expedita dolor nemo unde autem, tenetur nobis debitis deleniti.
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae optio voluptates non et a ullam nam qui nihil aliquid fugit tempore expedita dolor nemo unde autem, tenetur nobis debitis deleniti.
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae optio voluptates non et a ullam nam qui nihil aliquid fugit tempore expedita dolor nemo unde autem, tenetur nobis debitis deleniti.
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae optio voluptates non et a ullam nam qui nihil aliquid fugit tempore expedita dolor nemo unde autem, tenetur nobis debitis deleniti.
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae optio voluptates non et a ullam nam qui nihil aliquid fugit tempore expedita dolor nemo unde autem, tenetur nobis debitis deleniti.
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae optio voluptates non et a ullam nam qui nihil aliquid fugit tempore expedita dolor nemo unde autem, tenetur nobis debitis deleniti.
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae optio voluptates non et a ullam nam qui nihil aliquid fugit tempore expedita dolor nemo unde autem, tenetur nobis debitis deleniti.
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae optio voluptates non et a ullam nam qui nihil aliquid fugit tempore expedita dolor nemo unde autem, tenetur nobis debitis deleniti.
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae optio voluptates non et a ullam nam qui nihil aliquid fugit tempore expedita dolor nemo unde autem, tenetur nobis debitis deleniti.
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae optio voluptates non et a ullam nam qui nihil aliquid fugit tempore expedita dolor nemo unde autem, tenetur nobis debitis deleniti.
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae optio voluptates non et a ullam nam qui nihil aliquid fugit tempore expedita dolor nemo unde autem, tenetur nobis debitis deleniti.
+                it. Quae optio voluptates non et a ullam nam qui nihil aliquid fugit tempore expedita dolor nemo unde autem, tenetur nobis debitis deleniti.
                     Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae optio voluptates non et a ullam nam qui nihil aliquid fugit tempore expedita dolor nemo unde autem, tenetur nobis debitis deleniti.
 
                 </p>
@@ -47,9 +19,15 @@
         </article>
         <article id="instruction_legenda" class="instruction_legenda" v-else>
             <section class="instruction_legenda-top">
-                <h1 class="instruction_legenda_h1">Legenda</h1>
-                <button class="instruction_documentation-button" @click="goToDocumentation">Documentatie</button>
+                <header class="instruction_legenda-header">
+                    <h1 class="instruction_legenda-h1">Legenda</h1>
+                    <Link class="instruction_legenda-close" :href="`/`"><SvgIcon :name="`close`" /></Link>
+                </header>
+                <button class="instruction_legenda-button" @click="goToDocumentation">Documentatie</button>
                 <button class="instruction_legenda-button" @click="goToLegenda">Legenda</button>
+            </section>
+            <section class="instruction_legenda-mid">
+                
             </section>
         </article>
     </div>
