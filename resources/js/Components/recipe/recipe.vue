167910e591ac7a810af57d353bf8fa6cf54c959d
Milou Geervliet <milouyg@MacBook-Air-van-Milou.local>
2025-04-14 12:10:04 +0200

    Heart will be +1 or -1 in the localstorage when you click on the favourite icon in the recipe page

--- resources/js/Components/recipe/recipe.vue
+++ resources/js/Components/recipe/recipe.vue
@@ -14,13 +14,17 @@
                 <h3 class="recipe__title">Test</h3>
                 <ul class="recipe__icons">
                     <li class="recipe__icon">
-                        <span class="recipe__span">
-                            8 <SvgIcon :name="`heart`"/>
-                        </span>
+                        <button id="safeRecipe" class="recipe__span" @click="toggleSafeCount">
+                            {{ safeCount }}
+                            <svg id="heartSvg" xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                                class="bi bi-heart-fill" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314" />
+                            </svg>
+                        </button>
                     </li>
                     <li class="recipe__icon">
                         <span class="recipe__span">
-                            15 min <SvgIcon :name="`timer`"/>
+                            15 min <SvgIcon :name="`timer`" />
                         </span>
                     </li>
                 </ul>
@@ -31,18 +35,50 @@
 </template>
 
 <script>
-
 import imageComponent from '../general/image/imageComponent.vue';
 import SvgIcon from '../general/icon/SvgIcon.vue';
 import { Link } from '@inertiajs/vue3';
 
 export default {
     name: "recipeList",
-    components:{
+    components: {
         imageComponent,
         SvgIcon,
-        Link
-    }
-}
-
+        Link,
+    },
+    data() {
+        return {
+            safeCount: 8,
+            originalSafeCount: 8,
+            isSafeCountChanged: false,
+        };
+    },
+    mounted() {
+        if (localStorage.getItem("safeRecipe")) {
+            this.safeCount = parseInt(localStorage.getItem("safeRecipe"));
+            this.isSafeCountChanged = true;
+            this.updateHeartColor(); 
+        }
+    },
+    methods: {
+        toggleSafeCount() {
+            if (this.isSafeCountChanged) {
+                this.safeCount = this.originalSafeCount;
+                localStorage.removeItem("safeRecipe");
+                this.isSafeCountChanged = false;
+            } else {
+                this.safeCount = this.safeCount + 1;
+                localStorage.setItem("safeRecipe", this.safeCount);
+                this.isSafeCountChanged = true;
+            }
+            this.updateHeartColor();
+        },
+        updateHeartColor() {
+            const heartSvg = document.getElementById("heartSvg");
+            if (heartSvg) {
+                heartSvg.style.fill = this.isSafeCountChanged ? "red" : "currentColor";
+            }
+        },
+    },
+};
 </script>
