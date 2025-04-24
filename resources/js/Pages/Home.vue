7142948ada954ce14839000373c9c6227a38745b
Milou Geervliet <milouyg@MacBook-Air-van-Milou.local>
2025-04-24 10:30:01 +0200

    Search balk works now in the home and it also recommends recipes when you type in the search balk

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -5,11 +5,17 @@
                 <h1 class="yummies_header-h1"> {{ data.title }} </h1>
                 <label class="yummies_span">
                     <SvgIcon :name="`search`" />
-                    <input type="text" placeholder="Zoek verschillende kookstijlen" class="yummies_input"
-                        ref="searchField" id="yummies-input" />
+                    <input v-model="searchTerm" @input="generateSuggestions" @keyup.enter="performSearch" type="text"
+                        placeholder="Zoek verschillende kookstijlen" class="yummies_input" ref="searchField"
+                        id="yummies-input" />
+                    <ul v-if="suggestions.length > 0" class="search-suggestions">
+                        <li v-for="suggestion in suggestions" :key="suggestion.urlName"
+                            @click="selectSuggestion(suggestion)" class="search-suggestion-item">
+                            {{ suggestion.name }}
+                        </li>
+                    </ul>
                 </label>
-                <!-- <h2 class="yummies_header-h2"> {{ data.headerH2 }} </h2> -->
-            </div>
+                </div>
             <div class="yummies_navigation">
                 <Navigation @categorySelected="setSelectedCategory" />
             </div>
@@ -55,14 +61,27 @@ export default {
             isScrollable: false,
             footerVisible: true,
             isScrolling: false,
+            searchTerm: '',
+            suggestions: [],   // De zoeksuggesties
         };
     },
     computed: {
         filteredRecipes() {
-            if (!this.selectedCategory || this.selectedCategory === 'all') {
+            const searchTermLower = this.searchTerm.toLowerCase();
+
+            if (!searchTermLower && (!this.selectedCategory || this.selectedCategory === 'all')) {
                 return this.data.recipes;
             }
-            return this.data.recipes.filter(recipe => recipe.type === this.selectedCategory);
+            return this.data.recipes.filter(recipe => {
+                const nameRecipe = recipe.name.toLowerCase().includes(searchTermLower);
+                const nameType = recipe.type.toLowerCase().includes(searchTermLower);
+                const description = recipe.description.toLowerCase().includes(searchTermLower);
+                const ingredients = recipe.ingredients.some(ing => ing.name.toLowerCase().includes(searchTermLower));
+                const keywords = recipe.keywords && recipe.keywords.some(keyword => keyword.toLowerCase().includes(searchTermLower));
+                const category = !this.selectedCategory || this.selectedCategory === 'all' || recipe.type === this.selectedCategory;
+
+                return category && (nameRecipe || nameType || description || ingredients || keywords);
+            });
         }
     },
     mounted() {
@@ -107,6 +126,43 @@ export default {
                 this.isScrolling = false;
                 this.checkScrollability();
             }, 100);
+        },
+
+        generateSuggestions() {
+            const searchTermLower = this.searchTerm.toLowerCase();
+            if (!searchTermLower) {
+                this.suggestions = [];
+                return;
+            }
+
+            this.suggestions = this.data.recipes
+                .filter(recipe =>
+                    recipe.name.toLowerCase().includes(searchTermLower) ||
+                    recipe.type.toLowerCase().includes(searchTermLower) ||
+                    recipe.description.toLowerCase().includes(searchTermLower) ||
+                    recipe.ingredients.some(ing => ing.name.toLowerCase().includes(searchTermLower)) ||
+                    (recipe.keywords && recipe.keywords.some(keyword => keyword.toLowerCase().includes(searchTermLower)))
+                )
+                .slice(0, 5); // Limit the suggestions to 5
+        },
+
+        performSearch() {
+        // The filtering is already done in filteredRecipes,
+        // here we only need to hide the suggestions.
+            this.suggestions = [];
+        },
+
+        selectSuggestion(suggestion) {
+            // Fill in the searchterm
+            this.searchTerm = suggestion.name;
+            // Hide the suggestions
+            this.performSearch();
+        },
+
+        setSelectedCategory(category) {
+            this.selectedCategory = category;
+            this.searchTerm = '';
+            this.suggestions = [];
         },
     }
 };
