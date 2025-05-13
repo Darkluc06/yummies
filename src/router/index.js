3c39b817d83e93160eb39c3dac1886ad435675d9
Jacob Wassall-jamieson <jacob@thewj5.com>
2025-05-13 14:15:26 +0200

    fixed legenda page

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import Home from '../views/Home.vue';
 import DetailPage from '../views/recipes/detailPage.vue';
 import Documentation from '@/views/documentation/documentation.vue';
 import Favorite from '@/views/favorite/favorite.vue';
-import Legenda from '@/Components/legenda/legenda.vue';
+import Legenda from '@/views/legenda/legenda.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
--- src/views/legenda/legenda.vue
+++ src/views/legenda/legenda.vue
@@ -1,124 +1,129 @@
 <template>
     <DesktopNavigation :title="'Kook Diagram'" />
-    <article id="instruction_legenda" class="instruction_legenda">
-        <section class="instruction_legenda-top">
-            <header class="instruction_legenda-header">
-                <h1 class="instruction_legenda-h1"> {{ instruction.legenda.title }} </h1>
-                <Link class="instruction_legenda-close" :href="`/`">
-                <SvgIcon :name="`close`" />
-                </Link>
-            </header>
-            <div class="instruction__buttonWrapper">
-                <RouterLink class="instruction_legenda-button" activeClass="instruction_legenda-button--active" :to="'/documentation'">Documentatie</RouterLink>
-                <RouterLink class="instruction_legenda-button" activeClass="instruction_legenda-button--active" :to="'/legenda'">Legenda</RouterLink>
-            </div>
-        </section>
-        <section class="instruction_legenda-mid">
-            <summary class="instruction_legenda-summary"> {{ instruction.legenda.summary }} </summary>
-            <div class="instruction_containers">
-                <ul class="instruction_container-icons icons-condition">
-                    <li class="instruction_icons" v-for="(iconImg, index) in iconsCondition" :key="index"
-                        v-if="iconsCondition">
-                        <img class="instruction_icon" :src="getImgUrl(iconImg.src)" :alt="iconImg.alt" />
-                        <span class="instruction_icon-label">{{ iconImg.label }}</span>
-                    </li>
-                </ul>
-                <ul class="instruction_container-icons icons-xyz">
-                    <li class="instruction_icons" v-for="(iconImg, index) in iconsXYZ" :key="index" v-if="iconsXYZ">
-                        <img class="instruction_icon" :src="getImgUrl(iconImg.src)" :alt="iconImg.alt" />
-                        <span class="instruction_icon-label">{{ iconImg.label }}</span>
-                    </li>
-                </ul>
-                <ul class="instruction_container-icons icons-circles">
-                    <li class="instruction_icons" v-for="(iconImg, index) in iconsCircles" :key="index"
-                        v-if="iconsCircles">
-                        <img class="instruction_icon" :src="getImgUrl(iconImg.src)" :alt="iconImg.alt" />
-                        <span class="instruction_icon-label">{{ iconImg.label }}</span>
-                    </li>
-                </ul>
-                <ul class="instruction_container-icons icons-diamonds">
-                    <li class="instruction_icons" v-for="(iconImg, index) in iconsDiamonds" :key="index"
-                        v-if="iconsDiamonds">
-                        <img class="instruction_icon" :src="getImgUrl(iconImg.src)" :alt="iconImg.alt" />
-                        <span class="instruction_icon-label">{{ iconImg.label }}</span>
-                    </li>
-                </ul>
-                <ul class="instruction_container-icons icons-intensity">
-                    <p class="intensity-explanation">Intensiteit kan worden aangegeven op cond en acties van 0 naar
-                        5.
-                        Waar 5 het intens is en 0 niet gedefinieerd</p>
-                    <li class="instruction_icons" v-for="(iconImg, index) in iconsIntensity" :key="index"
-                        v-if="iconsIntensity">
-                        <img class="instruction_icon" :src="getImgUrl(iconImg.src)" :alt="iconImg.alt" />
-                        <span class="instruction_icon-label">{{ iconImg.label }}</span>
-                    </li>
-                </ul>
-                <ul class="instruction_container-icons icons-arrows">
-                    <p class="arrows-explanation">Een pijl, geeft aan dat alle elementen van de voorgaande moeten
-                        worden
-                        verplaats naar een ander instrumen</p>
-                    <li class="instruction_icons" v-for="(iconImg, index) in iconsArrows" :key="index"
-                        v-if="iconsArrows">
-                        <img class="instruction_icon" :src="getImgUrl(iconImg.src)" :alt="iconImg.alt" />
-                        <span class="instruction_icon-label">{{ iconImg.label }}</span>
-                    </li>
-                </ul>
-            </div>
-            <figure class="instruction_figure">
-                <p class="instruction_figure-p"> De onderstaande afbeelding geeft aan dat je iets tijdelijk blijvend
-                    moet
-                    doen
-                </p>
-                <img src="./../../../../public/img/iets_blijven_doen.webp" alt="" class="instruction_img-keep-doing">
-            </figure>
-            <div class="instruction_ingredients">
-                <p class="instruction_ingredients-p">Verschillende onderdelen (moeten) afgebeeld worden in
-                    (kleur)vakken,
-                    zodat je
-                    makkelijk kan zien
-                    wat bepaalde stappen doen. Het item naam en symbolen in het vak blijven wit. Vervolgens staan de
-                    ingredienten op volgorde van wat er
-                    als eerst in gaat en op volgorde van kleur.</p>
-                <img src="./../../../../public/img/colors.webp" alt="" class="instruction_ingredients-img">
-                <p class="instruction_ingredients-p">
-                    In de ingredienten staan eerst de hoeveelheid. Altijd in eenheiden, grammen, liters, eetlepels
-                    of thee lepels. En dan de naam. Wanneer een ingredient in twee blokken voorkomt
-                    komt bij zonder kleur boven aan
-                </p>
-                <div class="instruction_ingredients-container">
-                    <h2 class="instruction_ingredients-h2">Ingrediënten:</h2>
-                    <ul class="instruction_ingredients">
-                        <li class="instruction_ingredients-ingredient blue"
-                            v-for="(ingredient, index) in ingredientsBlue" :key="index">
-                            <input class="instruction_ingredients-checkbox" type="checkbox" />
-                            <span>{{ ingredient.name }} {{ ingredient.amount }} {{ ingredient.unit }}</span>
+    <div class="instruction">
+        <article id="instruction_legenda" class="instruction_legenda">
+            <section class="instruction_legenda-top">
+                <header class="instruction_legenda-header">
+                    <h1 class="instruction_legenda-h1"> {{ instruction.legenda.title }} </h1>
+                    <Link class="instruction_legenda-close" :href="`/`">
+                    <SvgIcon :name="`close`" />
+                    </Link>
+                </header>
+                <div class="instruction__buttonWrapper">
+                    <RouterLink class="instruction_legenda-button" activeClass="instruction_legenda-button--active"
+                        :to="'/documentation'">Documentatie</RouterLink>
+                    <RouterLink class="instruction_legenda-button" activeClass="instruction_legenda-button--active"
+                        :to="'/legenda'">Legenda</RouterLink>
+                </div>
+            </section>
+            <section class="instruction_legenda-mid">
+                <summary class="instruction_legenda-summary"> {{ instruction.legenda.summary }} </summary>
+                <div class="instruction_containers">
+                    <ul class="instruction_container-icons icons-condition">
+                        <li class="instruction_icons" v-for="(iconImg, index) in iconsCondition" :key="index"
+                            v-if="iconsCondition">
+                            <img class="instruction_icon" :src="getImgUrl(iconImg.src)" :alt="iconImg.alt" />
+                            <span class="instruction_icon-label">{{ iconImg.label }}</span>
                         </li>
                     </ul>
-                    <ul class="instruction_ingredients">
-                        <li class="instruction_ingredients-ingredient green"
-                            v-for="(ingredient, index) in ingredientsGreen" :key="index">
-                            <input class="instruction_ingredients-checkbox" type="checkbox" />
-                            <span>{{ ingredient.name }} {{ ingredient.amount }} {{ ingredient.unit }}</span>
+                    <ul class="instruction_container-icons icons-xyz">
+                        <li class="instruction_icons" v-for="(iconImg, index) in iconsXYZ" :key="index" v-if="iconsXYZ">
+                            <img class="instruction_icon" :src="getImgUrl(iconImg.src)" :alt="iconImg.alt" />
+                            <span class="instruction_icon-label">{{ iconImg.label }}</span>
                         </li>
                     </ul>
-                    <ul class="instruction_ingredients">
-                        <li class="instruction_ingredients-ingredient yellow"
-                            v-for="(ingredient, index) in ingredientsYellow" :key="index">
-                            <input class="instruction_ingredients-checkbox" type="checkbox" />
-                            <span>{{ ingredient.name }} {{ ingredient.amount }} {{ ingredient.unit }}</span>
+                    <ul class="instruction_container-icons icons-circles">
+                        <li class="instruction_icons" v-for="(iconImg, index) in iconsCircles" :key="index"
+                            v-if="iconsCircles">
+                            <img class="instruction_icon" :src="getImgUrl(iconImg.src)" :alt="iconImg.alt" />
+                            <span class="instruction_icon-label">{{ iconImg.label }}</span>
                         </li>
                     </ul>
-                    <ul class="instruction_ingredients">
-                        <li class="instruction_ingredients-ingredient white">
-                            <input class="instruction_ingredients-checkbox" type="checkbox" />
-                            <span>{{ ingredientsWhite.name }} {{ ingredientsWhite.amount }} {{
-                                ingredientsWhite.unit }}</span>
+                    <ul class="instruction_container-icons icons-diamonds">
+                        <li class="instruction_icons" v-for="(iconImg, index) in iconsDiamonds" :key="index"
+                            v-if="iconsDiamonds">
+                            <img class="instruction_icon" :src="getImgUrl(iconImg.src)" :alt="iconImg.alt" />
+                            <span class="instruction_icon-label">{{ iconImg.label }}</span>
+                        </li>
+                    </ul>
+                    <ul class="instruction_container-icons icons-intensity">
+                        <p class="intensity-explanation">Intensiteit kan worden aangegeven op cond en acties van 0 naar
+                            5.
+                            Waar 5 het intens is en 0 niet gedefinieerd</p>
+                        <li class="instruction_icons" v-for="(iconImg, index) in iconsIntensity" :key="index"
+                            v-if="iconsIntensity">
+                            <img class="instruction_icon" :src="getImgUrl(iconImg.src)" :alt="iconImg.alt" />
+                            <span class="instruction_icon-label">{{ iconImg.label }}</span>
+                        </li>
+                    </ul>
+                    <ul class="instruction_container-icons icons-arrows">
+                        <p class="arrows-explanation">Een pijl, geeft aan dat alle elementen van de voorgaande moeten
+                            worden
+                            verplaats naar een ander instrumen</p>
+                        <li class="instruction_icons" v-for="(iconImg, index) in iconsArrows" :key="index"
+                            v-if="iconsArrows">
+                            <img class="instruction_icon" :src="getImgUrl(iconImg.src)" :alt="iconImg.alt" />
+                            <span class="instruction_icon-label">{{ iconImg.label }}</span>
                         </li>
                     </ul>
                 </div>
-            </div>
-        </section>
-    </article>
+                <figure class="instruction_figure">
+                    <p class="instruction_figure-p"> De onderstaande afbeelding geeft aan dat je iets tijdelijk blijvend
+                        moet
+                        doen
+                    </p>
+                    <img src="/public/img/iets_blijven_doen.webp" alt="" class="instruction_img-keep-doing">
+                </figure>
+                <div class="instruction_ingredients">
+                    <p class="instruction_ingredients-p">Verschillende onderdelen (moeten) afgebeeld worden in
+                        (kleur)vakken,
+                        zodat je
+                        makkelijk kan zien
+                        wat bepaalde stappen doen. Het item naam en symbolen in het vak blijven wit. Vervolgens staan de
+                        ingredienten op volgorde van wat er
+                        als eerst in gaat en op volgorde van kleur.</p>
+                    <img src="/public/img/colors.webp" alt="" class="instruction_ingredients-img">
+                    <p class="instruction_ingredients-p">
+                        In de ingredienten staan eerst de hoeveelheid. Altijd in eenheiden, grammen, liters, eetlepels
+                        of thee lepels. En dan de naam. Wanneer een ingredient in twee blokken voorkomt
+                        komt bij zonder kleur boven aan
+                    </p>
+                    <div class="instruction_ingredients-container">
+                        <h2 class="instruction_ingredients-h2">Ingrediënten:</h2>
+                        <ul class="instruction_ingredients">
+                            <li class="instruction_ingredients-ingredient blue"
+                                v-for="(ingredient, index) in ingredientsBlue" :key="index">
+                                <input class="instruction_ingredients-checkbox" type="checkbox" />
+                                <span>{{ ingredient.name }} {{ ingredient.amount }} {{ ingredient.unit }}</span>
+                            </li>
+                        </ul>
+                        <ul class="instruction_ingredients">
+                            <li class="instruction_ingredients-ingredient green"
+                                v-for="(ingredient, index) in ingredientsGreen" :key="index">
+                                <input class="instruction_ingredients-checkbox" type="checkbox" />
+                                <span>{{ ingredient.name }} {{ ingredient.amount }} {{ ingredient.unit }}</span>
+                            </li>
+                        </ul>
+                        <ul class="instruction_ingredients">
+                            <li class="instruction_ingredients-ingredient yellow"
+                                v-for="(ingredient, index) in ingredientsYellow" :key="index">
+                                <input class="instruction_ingredients-checkbox" type="checkbox" />
+                                <span>{{ ingredient.name }} {{ ingredient.amount }} {{ ingredient.unit }}</span>
+                            </li>
+                        </ul>
+                        <ul class="instruction_ingredients">
+                            <li class="instruction_ingredients-ingredient white">
+                                <input class="instruction_ingredients-checkbox" type="checkbox" />
+                                <span>{{ ingredientsWhite.name }} {{ ingredientsWhite.amount }} {{
+                                    ingredientsWhite.unit }}</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+        </article>
+    </div>
+    <FooterComponent :class="{ hidden: !footerVisible }"></FooterComponent>
 </template>
 
 <script>
@@ -137,7 +142,6 @@ export default {
     },
     data() {
         return {
-            showDocumentation: true,
             instruction: json["instruction"],
             icons: json["instruction"]["legenda"]["imgs"],
 
@@ -168,12 +172,6 @@ export default {
         window.removeEventListener('scroll', this.handleScroll);
     },
     methods: {
-        goToDocumentation() {
-            this.showDocumentation = true;
-        },
-        goToLegenda() {
-            this.showDocumentation = false;
-        },
         getImgUrl(filename) {
             return `/img/${filename}`;
         },
