32c0a415848ebd72ab0899c011453fd98f621f9a
Jacob Wassall-jamieson <jacob@thewj5.com>
2025-05-15 09:53:06 +0200

    added timeline html

---
+++ src/Components/cookingDiagram/timeline.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="cookingDiagram__lineWrapper">
+        <figure class="cookingDiagram__line"></figure>
+    </div>
+    <figure class="cookingDiagram__timeline" :class="`steps--${steps}`"></figure>
+</template>
+
+<script>
+
+export default {
+    props: {
+        steps: {
+            type: String,
+            default: "1"
+        }
+    }
+}
+
+</script>
