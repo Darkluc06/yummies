7dd52c93eacd6b361850ae6d2a5391f76994017e
Jacob Wassall-jamieson <jacob@thewj5.com>
2025-05-15 10:26:40 +0200

    added time line arch

--- src/Components/cookingDiagram/timeline.vue
+++ src/Components/cookingDiagram/timeline.vue
@@ -1,7 +1,9 @@
 <template>
-    <div class="cookingDiagram__lineWrapper">
-        <figure class="cookingDiagram__line cookingDiagram__line--active"></figure>
-        <figure class="cookingDiagram__timeline" :class="`steps--${steps}`"></figure>
+    <div class="cookingDiagram__column">
+        <div class="cookingDiagram__lineWrapper">
+            <figure class="cookingDiagram__line cookingDiagram__line--active"></figure>
+            <figure class="cookingDiagram__timeline" :class="`steps--${steps}`"></figure>
+        </div>
     </div>
 </template>
 
