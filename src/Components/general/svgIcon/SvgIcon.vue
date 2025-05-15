ec8cc25fc2a38c281c94aa943c9509ede390a4b9
Luc <luc@specialmomentsgroup.com>
2025-05-15 09:24:52 +0200

    commit saved

--- src/Components/general/svgIcon/SvgIcon.vue
+++ src/Components/general/svgIcon/SvgIcon.vue
@@ -1,5 +1,5 @@
 <template>
-    <svg
+  <svg
       v-if="currentPath"
       :class="className"
       :width="size"
@@ -7,11 +7,13 @@
       xmlns="http://www.w3.org/2000/svg"
       :viewBox="iconViewBox"
       :fill="color"
-    >
-      <path :d="currentPath" />
-    </svg>
-  </template>
-  
+      :stroke="stroke"
+      :stroke-width="strokeWidth"
+  >
+    <path :d="currentPath" />
+  </svg>
+</template>
+
 <script>
 import { computed } from 'vue';
 import svgPaths from '@/assets/icons/icons.json';
@@ -24,22 +26,29 @@ export default {
     },
     size: {
       type: [String, Number],
-      default: 24,
+      default: '2.4em',
     },
     color: {
       type: String,
       default: 'currentColor',
+    },
+    stroke: {
+      type: String,
+      default: 'none',
+    },
+    strokeWidth: {
+      type: [String, Number],
+      default: 'none',
     },
     className: {
       type: String,
       default: 'icon',
     },
   },
-  setup(props) {
-    const currentPath = computed(() => svgPaths[props.name]?.[0]?.path || null);
-    const iconViewBox = computed(() => svgPaths[props.name]?.[0]?.viewbox || null);
-    return { currentPath, iconViewBox };
-  },
-};
+    setup(props) {
+      const currentPath = computed(() => svgPaths[props.name]?.[0]?.path || null);
+      const iconViewBox = computed(() => svgPaths[props.name]?.[0]?.viewbox || null);
+      return { currentPath, iconViewBox };
+    },
+  };
 </script>
-  
