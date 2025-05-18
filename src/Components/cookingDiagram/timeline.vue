b33c284422bb420b50dadd53e8bcc2f6c0d6ce07
Jacob Wassall-jamieson <jacob@thewj5.com>
2025-05-18 21:22:49 +0200

    fixed time line styling

--- src/Components/cookingDiagram/timeline.vue
+++ src/Components/cookingDiagram/timeline.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="cookingDiagram__column">
+        <p class="cookingDiagram__timeline--time">{{ time }}</p>
         <div class="cookingDiagram__lineWrapper">
             <figure class="cookingDiagram__line cookingDiagram__line--active"></figure>
             <ul class="cookingDiagram__timeline" :class="`steps--${steps}`">
                 <figure class="cookingDiagram__timeline--line"></figure>
                 <li class="cookingDiagram__timeline--divider cookingDiagram__timeline--divider-top"></li>
                 <li class="cookingDiagram__timeline--divider cookingDiagram__timeline--divider-bottom"></li>
-                <p class="cookingDiagram__timeline--time">{{ time }}</p>
             </ul>
         </div>
     </div>
@@ -20,7 +20,7 @@ export default {
             type: String,
             default: "1"
         },
-        time:{
+        time: {
             type: String,
             default: "1 min",
             required: false
