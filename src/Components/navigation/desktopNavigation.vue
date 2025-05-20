32fafac32d25f93acf698bdb71f741846398d8b2
Luc <luc@specialmomentsgroup.com>
2025-05-20 23:31:55 +0200

    max width navbar

--- src/Components/navigation/desktopNavigation.vue
+++ src/Components/navigation/desktopNavigation.vue
@@ -1,34 +1,36 @@
 <template>
     <nav class="desktopNav">
-        <RouterLink :to="`/`">
-            <h1 class="desktopNav__title">
-                {{ title }}
-            </h1>
-        </RouterLink>
-        <ul class="desktopNav__list">
-            <li class="desktopNav__listItem">
-                <RouterLink :to="`/`" class="desktopNav__link" activeClass="desktopNav__link--active">
-                    <SvgIcon :name="'book'" />
-                    Recepten
-                </RouterLink>
-            </li>
-            <li class="desktopNav__listItem">
-                <figure class="arrow-container" v-if="this.renderArrow()">
-                    <img class="arrow-container--img" src="/img/checkMe.webp" alt="">
-                    <p class="arrow-container--p">Check me</p>
-                </figure>
-                <RouterLink :to="`/documentation`" class="desktopNav__link" activeClass="desktopNav__link--active">
-                    <SvgIcon :name="'document'" />
-                    Documentatie
-                </RouterLink>
-            </li>
-            <li class="desktopNav__listItem">
-                <RouterLink :to="`/favorite`" class="desktopNav__link" activeClass="desktopNav__link--active">
-                    <SvgIcon :name="'heart'" />
-                    Favorieten
-                </RouterLink>
-            </li>
-        </ul>
+        <div>
+            <RouterLink :to="`/`">
+                <h1 class="desktopNav__title">
+                    {{ title }}
+                </h1>
+            </RouterLink>
+            <ul class="desktopNav__list">
+                <li class="desktopNav__listItem">
+                    <RouterLink :to="`/`" class="desktopNav__link" activeClass="desktopNav__link--active">
+                        <SvgIcon :name="'book'" />
+                        Recepten
+                    </RouterLink>
+                </li>
+                <li class="desktopNav__listItem">
+                    <figure class="arrow-container" v-if="this.renderArrow()">
+                        <img class="arrow-container--img" src="/img/checkMe.webp" alt="">
+                        <p class="arrow-container--p">Check me</p>
+                    </figure>
+                    <RouterLink :to="`/documentation`" class="desktopNav__link" activeClass="desktopNav__link--active">
+                        <SvgIcon :name="'document'" />
+                        Documentatie
+                    </RouterLink>
+                </li>
+                <li class="desktopNav__listItem">
+                    <RouterLink :to="`/favorite`" class="desktopNav__link" activeClass="desktopNav__link--active">
+                        <SvgIcon :name="'heart'" />
+                        Favorieten
+                    </RouterLink>
+                </li>
+            </ul>
+        </div>
     </nav>
 </template>
 
@@ -39,17 +41,17 @@ import SvgIcon from '../general/icon/SvgIcon.vue';
 
 export default {
     props: {
-        title: String
+        title: String,
     },
     components: {
         RouterLink,
-        SvgIcon
+        SvgIcon,
     },
     methods: {
         renderArrow() {
-            return localStorage.getItem("arrow") !== "true";
-        }
-    }
-}
+            return localStorage.getItem('arrow') !== 'true';
+        },
+    },
+};
 
 </script>
