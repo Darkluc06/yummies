45c3d6dc1abee2bba0199c8a429386f23c606bfe
Milou <33333@ma-web.nl.com>
2025-06-02 11:10:33 +0200

    Added img tag to missing icons

--- src/Components/modal/modal.vue
+++ src/Components/modal/modal.vue
@@ -105,7 +105,7 @@ export default {
                             iconToAdd = {
                                 src: originalSrc,
                                 alt: 'Laat zien dat je van lijn moet wisselen',
-                                label: 'Actie'
+                                label: 'Verplaats ingredient'
                             };
                         }
 
